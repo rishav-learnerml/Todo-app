876cbcf3c5baac66acdb7aa43e705ec8f1904225
rishav-learnerml <rishavbio1@gmail.com>
2020-07-07 15:12:06 +0530

    Updates

---
+++ precache-manifest.276642f8f170dace876426991c19d263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6595726fbab272a11a886263311b7b3c",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "bb8e7ced4c0b8dcc71fb",
+    "url": "/Todo-app/static/css/main.6b7f08e3.chunk.css"
+  },
+  {
+    "revision": "c6cf03e951cfcec6af6d",
+    "url": "/Todo-app/static/js/2.d8b7b97f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.d8b7b97f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8e7ced4c0b8dcc71fb",
+    "url": "/Todo-app/static/js/main.eff7646c.chunk.js"
+  },
+  {
+    "revision": "9f48de200ba6786cff8a",
+    "url": "/Todo-app/static/js/runtime-main.e9bd9999.js"
+  },
+  {
+    "revision": "f90bc74317b28114c7022820e4c2f251",
+    "url": "/Todo-app/static/media/logo.f90bc743.png"
+  }
+]);
